f68a9d2c94cafa1586c5f1beb301ea8f38c06f85
Roshan Shrivastava <gulla074@gmail.com>
2022-05-09 14:37:38 +0530

    Added

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2022 Student Registration Portal
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+    <style> .col-md-12{text-align: center;} </style>
