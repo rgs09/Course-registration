4d65c8150a85347bc844ea4c30abcc2cd1c17a78
RoshanShrivastava <gulla074@gmail.com>
2022-07-03 10:57:56 +0530

    initial build

--- admin/change-password.php
+++ admin/change-password.php
@@ -7,7 +7,7 @@ if(strlen($_SESSION['alogin'])==0)
 header('location:index.php');
 }
 else{
-date_default_timezone_set('Asia/Kolkata');// change according timezone
+date_default_timezone_set('Asia/Kolkata');
 $currentTime = date( 'd-m-Y h:i:s A', time () );
 
 
